ed829a9a4261c02d33cd60afa6138eed13aac6fe
swelanauguste <swelanauguste@gmail.com>
2022-04-15 06:34:17 +0000

    debug off

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -12,9 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wyqr1k%5#c22)r^5(4-zkh=(p_iol(2377*#e73zvo3oy7*_tl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://kingship.loca.lt"]
 
 
 # Application definition
